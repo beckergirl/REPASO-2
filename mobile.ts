63d84d5190ced2cb52b36b0eea79d64568a18d21
beckergirl <116119849+beckergirl@users.noreply.github.com>
2022-10-26 13:51:47 +0200

    subida reto 2, mobile

---
+++ mobile.ts
@@ -0,0 +1,73 @@
+import { myMobiles } from "./mobileTest"
+
+export class Mobile{
+
+    private name: string;
+    private trademark: string;
+    private model: string;
+    private color: string;
+    private price: number;
+
+    constructor(name: string, trademark: string, model: string, color: string, price: number){
+
+        this.name = name;
+        this.trademark = trademark;
+        this.model = model;
+        this.color = color;
+        this.price = price;
+    }
+
+    public setName(){
+
+        return this.name;
+    }
+    public getName(){
+
+        return this.name;
+    }
+    public setTrademark(){
+
+        return this.trademark;
+    }
+    public getTrademark(){
+
+        return this.trademark;
+    }
+    public setModel(){
+
+        return this.model;
+    }
+    public getModel(){
+
+        return this.model;
+    }
+    public setColor(){
+
+        return this.color;
+    }
+    public getColor(){
+
+        return this.color;
+    }
+    public setPrice(){
+
+        return this.price;
+    }
+    public getPrice(){
+
+        return this.price;
+    }
+    public printMobiles(mobiles: any[]){
+
+        for(let i = 0; i < myMobiles.length; i++){
+
+            return mobiles.push(myMobiles[i]);
+
+        }
+    }
+  }
+
+
+
+let telefonoMiCasa: Mobile = new Mobile("nokia5","Nokia","modelo Z","negro", 543);
+console.log(telefonoMiCasa);
---
+++ mobileTest.ts
@@ -0,0 +1,18 @@
+import { Mobile } from "./mobile"
+
+let nokia: Mobile = new Mobile ("nokia 3310", "Nokia", "negro","vintage 3310",90);
+console.log(nokia);
+
+let iphone: Mobile = new Mobile ("iphone 14Pro", "apple", "plateado","14 pro", 1.469);
+console.log(iphone);
+
+let samsung: Mobile = new Mobile ("samsung Galaxy", "samsung", "blanco","galaxy Zlip4",1099);
+console.log(samsung);
+
+
+export let myMobiles = [ nokia, iphone, samsung];
+console.log(myMobiles);
+
+
+
+
